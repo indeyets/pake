4f1650f71906167f2834ea1c17ed79b825f7aed7
Fabien Potencier <fabien.potencier@sensio.com>
2007-07-26 16:23:28 +0000

    pake: changed Phing colorization from our hack to the default phing.listener.AnsiColorLogger (errors are in red which is better to spot problems when they occur)

--- lib/pake/tasks/pakePhingTask.class.php
+++ lib/pake/tasks/pakePhingTask.class.php
@@ -48,31 +48,15 @@ class pakePhingTask
       $args[] = $target;
     }
 
+    $args[] = '-logger';
+    $args[] = 'phing.listener.AnsiColorLogger';
+
     Phing::startup();
     Phing::setProperty('phing.home', getenv('PHING_HOME'));
 
-    ob_start(array('pakePhingTask', 'colorize'), 2);
     $m = new pakePhing();
     $m->execute($args);
     $m->runBuild();
-    ob_end_clean();
-  }
-
-  public static function colorize($text)
-  {
-    return preg_replace(array(
-      '#\[(.+?)\]#',
-      '#{{PHP Error}}#e',
-      '#({{.+?}})#e',
-      '#(\+ [^ ]+)#e',
-      '#{{(.+?)}}#',
-    ), array(
-      '{{$1}}',
-      'pakeColor::colorize("(PHP Error)", "ERROR")',
-      'pakeColor::colorize("$1", "INFO")',
-      'pakeColor::colorize("$1", "INFO")',
-      '[$1]',
-    ), $text);
   }
 }
 
