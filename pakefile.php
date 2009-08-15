ff243b0afafe669379506841a99305a52314c271
Alexey Zakhlestin <indeyets@gmail.com>
2009-08-16 02:30:06 +0400

    task for generating phar-packed version of pake + pear-style indentation of Pakefile.php

--- pakefile.php
+++ pakefile.php
@@ -7,6 +7,9 @@ pake_import('pear');
 pake_desc('create a single file with all pake classes');
 pake_task('compact');
 
+pake_desc('create an executable PHAR-archive of Pake');
+pake_task('phar');
+
 pake_desc('release a new pake version');
 pake_task('release');
 
@@ -14,7 +17,7 @@ pake_task('foo');
 
 function run_foo($task, $args)
 {
-  throw new Exception('test');
+    throw new Exception('test');
 }
 
 /* tasks */
@@ -25,93 +28,95 @@ function run_foo($task, $args)
  */
 function run_compact($task, $args)
 {
-  $plugins = $args;
+    $plugins = $args;
 
-  // merge all files
-  $content = '';
-  $files = pakeFinder::type('file')->name('*.class.php')->in(getcwd().'/lib/pake');
-  $files[] = getcwd().'/bin/pake.php';
-  foreach ($args as $plugin_name)
-  {
-    $files[] = getcwd().'/lib/pake/tasks/pake'.$plugin_name.'Task.class.php';
-  }
+    // merge all files
+    $content = '';
+    $files = pakeFinder::type('file')->name('*.class.php')->in(getcwd().'/lib/pake');
+    $files[] = getcwd().'/bin/pake.php';
+    foreach ($args as $plugin_name) {
+        $files[] = getcwd().'/lib/pake/tasks/pake'.$plugin_name.'Task.class.php';
+    }
 
-  foreach ($files as $file)
-  {
-    $content .= file_get_contents($file);
-  }
+    foreach ($files as $file) {
+        $content .= file_get_contents($file);
+    }
 
-  // strip require_once statements
-  $content = preg_replace('/^\s*require_once[^;]+;/m', '', $content);
+    // strip require_once statements
+    $content = preg_replace('/^\s*require_once[^;]+;/m', '', $content);
 
-  // replace windows and mac format with unix format
-  $content = str_replace(array("\r\n"), "\n", $content);
+    // replace windows and mac format with unix format
+    $content = str_replace(array("\r\n"), "\n", $content);
 
-  // strip php tags
-  $content = preg_replace(array("/<\?php/", "/<\?/", "/\?>/"), '', $content);
+    // strip php tags
+    $content = preg_replace(array("/<\?php/", "/<\?/", "/\?>/"), '', $content);
 
-  // replace multiple new lines with a single newline
-  $content = preg_replace(array("/\n\s+\n/s", "/\n+/s"), "\n", $content);
+    // replace multiple new lines with a single newline
+    $content = preg_replace(array("/\n\s+\n/s", "/\n+/s"), "\n", $content);
 
-  $content = "<?php\n".trim($content)."\n";
+    $content = "<?php\n".trim($content)."\n";
 
-  file_put_contents(getcwd().'/bin/pake_runtime.php', $content);
+    file_put_contents(getcwd().'/bin/pake_runtime.php', $content);
 
-  // strip all comments
-  pake_strip_php_comments(getcwd().'/bin/pake_runtime.php');
+    // strip all comments
+    pake_strip_php_comments(getcwd().'/bin/pake_runtime.php');
+}
+
+function run_phar()
+{
+    $finder = pakeFinder::type('any')->ignore_version_control()->name('phar-stub.php', '*.class.php', 'init.php');
+    pakeArchive::createPharArchive($finder, dirname(__FILE__), 'pake.phar', 'phar-stub.php', null, true);
 }
 
 function run_create_pear_package($task, $args)
 {
-  if (!isset($args[0]) || !$args[0])
-  {
-    throw new pakeException('You must provide pake version to release (1.1.X for example).');
-  }
+    if (!isset($args[0]) || !$args[0]) {
+        throw new pakeException('You must provide pake version to release (1.1.X for example).');
+    }
 
-  $version = $args[0];
+    $version = $args[0];
 
-  // create a pear package
-  echo 'create pear package for version "'.$version."\"\n";
+    // create a pear package
+    echo 'create pear package for version "'.$version."\"\n";
 
-  pake_copy(getcwd().'/package.xml.tmpl', getcwd().'/package.xml');
+    pake_copy(getcwd().'/package.xml.tmpl', getcwd().'/package.xml');
 
-  // add class files
-  $class_files = pakeFinder::type('file')->prune('.svn')->not_name('/^pakeApp.class.php$/')->name('*.php')->relative()->in('lib');
-  $xml_classes = '';
-  foreach ($class_files as $file)
-  {
-    $dir_name  = dirname($file);
-    $file_name = basename($file);
-    $xml_classes .= '<file role="php" baseinstalldir="'.$dir_name.'" install-as="'.$file_name.'" name="lib/'.$file.'"/>'."\n";
-  }
+    // add class files
+    $class_files = pakeFinder::type('file')->prune('.svn')->not_name('/^pakeApp.class.php$/')->name('*.php')->relative()->in('lib');
+    $xml_classes = '';
+    foreach ($class_files as $file) {
+        $dir_name  = dirname($file);
+        $file_name = basename($file);
+        $xml_classes .= '<file role="php" baseinstalldir="'.$dir_name.'" install-as="'.$file_name.'" name="lib/'.$file.'"/>'."\n";
+    }
 
-  // replace tokens
-  pake_replace_tokens('package.xml', getcwd(), '##', '##', array(
-    'PAKE_VERSION' => $version,
-    'CURRENT_DATE' => date('Y-m-d'),
-    'CLASS_FILES'  => $xml_classes,
-  ));
-  pake_replace_tokens('lib/pake/pakeApp.class.php', getcwd(), 'const VERSION = \'', '\';', array('1.1.DEV' => "const VERSION = '$version';"));
+    // replace tokens
+    pake_replace_tokens('package.xml', getcwd(), '##', '##', array(
+        'PAKE_VERSION' => $version,
+        'CURRENT_DATE' => date('Y-m-d'),
+        'CLASS_FILES'  => $xml_classes,
+    ));
+    pake_replace_tokens('lib/pake/pakeApp.class.php', getcwd(), 'const VERSION = \'', '\';', array('1.1.DEV' => "const VERSION = '$version';"));
 
-  pakePearTask::run_pear($task, $args);
-  pake_remove('package.xml', getcwd());
-  pake_replace_tokens('lib/pake/pakeApp.class.php', getcwd(), 'const VERSION = \'', '\';', array($version => "const VERSION = '1.1.DEV';"));
+    pakePearTask::run_pear($task, $args);
+    pake_remove('package.xml', getcwd());
+    pake_replace_tokens('lib/pake/pakeApp.class.php', getcwd(), 'const VERSION = \'', '\';', array($version => "const VERSION = '1.1.DEV';"));
 }
 
 function run_release($task, $args)
 {
-  if (!isset($args[0]) || !$args[0])
-  {
-    throw new pakeException('You must provide pake version to release (1.1.X for example).');
-  }
+    if (!isset($args[0]) || !$args[0]) {
+        throw new pakeException('You must provide pake version to release (1.2.X for example).');
+    }
 
-  $version = $args[0];
+    $version = $args[0];
 
-  pakeSimpletestTask::run_test($task, array());
+    pakeSimpletestTask::run_test($task, array());
 
-  if ($task->is_verbose()) echo 'releasing pake version "'.$version."\"\n";
+    if ($task->is_verbose())
+        pake_echo('releasing pake version "'.$version.'"');
 
-  array_unshift($args, $version);
+    array_unshift($args, $version);
 
-  run_create_pear_package($task, $args);
+    run_create_pear_package($task, $args);
 }
---
+++ phar-stub.php
@@ -0,0 +1,6 @@
+<?php
+
+require './lib/pake/init.php';
+
+$pake = pakeApp::get_instance();
+$pake->run();
