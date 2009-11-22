c36d1096f12000bc8f06d68de502e1d2cba29c07
Alexey Zakhlestin <indeyets@gmail.com>
2009-11-22 18:48:22 +0300

    do not use shell for pear-packaging, if possible

--- lib/pake/tasks/pakePearTask.class.php
+++ lib/pake/tasks/pakePearTask.class.php
@@ -10,18 +10,32 @@
 
 class pakePearTask
 {
-  public static function import_default_tasks()
-  {
-    pake_desc('create a PEAR package');
-    pake_task('pakePearTask::pear');
-  }
-
-  public static function run_pear($task, $args)
-  {
-    $results = pake_sh('pear package');
-    if ($task->is_verbose())
+    public static function import_default_tasks()
     {
-      echo $results;
+        pake_desc('create a PEAR package');
+        pake_task('pakePearTask::pear_package');
     }
-  }
+
+    public static function run_pear_package($task, $args)
+    {
+        if (!class_exists('PEAR_Packager')) {
+            @include('PEAR/Packager.php');
+
+            if (!class_exists('PEAR_Packager')) {
+                // falling back to cli-call
+                $results = pake_sh('pear package');
+                if ($task->is_verbose())
+                {
+                    echo $results;
+                }
+                return;
+            }
+        }
+
+        $packager = new PEAR_Packager();
+        $packager->debug = 0; // silence output
+        $archive = $packager->package('package.xml', true);
+
+        pake_echo_action('pear+', $archive);
+    }
 }
