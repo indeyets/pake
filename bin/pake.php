1822e7569172842fac7fe888d14ffd3d723d6d57
Alexey Zakhlestin <indeyets@gmail.com>
2012-01-11 21:53:31 +0400

    fixed indentation

--- bin/pake.php
+++ bin/pake.php
@@ -1,13 +1,10 @@
 <?php
 
 // set classpath
-if (getenv('PHP_CLASSPATH'))
-{
-  set_include_path(dirname(__FILE__).'/../lib'.PATH_SEPARATOR.getenv('PHP_CLASSPATH').PATH_SEPARATOR.get_include_path());
-}
-else
-{
-  set_include_path(dirname(__FILE__).'/../lib'.PATH_SEPARATOR.get_include_path());
+if (getenv('PHP_CLASSPATH')) {
+    set_include_path(dirname(__FILE__).'/../lib'.PATH_SEPARATOR.getenv('PHP_CLASSPATH').PATH_SEPARATOR.get_include_path());
+} else {
+    set_include_path(dirname(__FILE__).'/../lib'.PATH_SEPARATOR.get_include_path());
 }
 
 require 'pake/init.php';
