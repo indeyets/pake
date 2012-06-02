cabe790c57a6c418973c8ea7f898c81d7803c5a8
Alexey Zakhlestin <indeyets@gmail.com>
2012-06-02 22:54:17 +0300

    check current-dir, at first. refs #19

--- lib/pake/pakeFunction.php
+++ lib/pake/pakeFunction.php
@@ -351,6 +351,7 @@ function pake_which($cmd)
 
     if ($is_windows) {
         $win_executable_extensions = explode(';', getenv('PATHEXT'));
+        array_unshift($win_executable_extensions, realpath(getcwd()));
     }
 
     $extension = pathinfo($cmd, PATHINFO_EXTENSION);
